a697b108dd3292b2f4a38e35ead653e7d2ed8e1e
gonzabenitez <gonza12596@yahoo.com>
2025-07-14 13:12:55 -0300

    Creación de la base de datos y lotes de prueba

---
+++ src/controller.js
@@ -0,0 +1,29 @@
+import {pool} from './database.js';
+
+class librosController{
+
+    async getAll(req, res) {
+        const [result] = await pool.query('SELECT * FROM libros');
+        res.json(result);
+    }
+
+    async add(req, res){
+        const libros = req.body;
+        const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, año_publicacion, isbn) VALUES (?, ?, ?, ?, ?)`, [libros.nombre, libros.autor, libros.categoria, libros.año_publicacion, libros.isbn])
+        res.json({"Id insertado": result.insertId});
+    }
+
+    async delete(req, res){
+        const libros = req.body;
+        const [result] = await pool.query(`DELETE FROM libros WHERE id=(?)`, [libros.isbn]);
+        res.json({"Registros Eliminados": result.affectedRows});
+    }
+    async update(req, res){
+        const libros = req.body;
+        const [result] = await pool.query(`UPDATE libros SET nombre=(?), autor=(?), categoria=(?), año_publicacion=(?), isbn=(?) WHERE id=(?)`, [libros.nombre, libros.autor, libros.categoria, libros.año_publicacion, libros.isbn, libros.id]);
+        res.json({"Registros Actualizados": result.changedRows});
+    }
+
+}
+
+export const libros = new librosController();
